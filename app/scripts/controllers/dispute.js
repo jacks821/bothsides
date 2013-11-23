05768202f23f7a240340aadfa5b49861eabe76e1
Nicholas Shook <nicholas.shook@gmail.com>
2013-11-23 14:22:55 -0800

    added tabs for disputes

--- app/scripts/controllers/dispute.js
+++ app/scripts/controllers/dispute.js
@@ -1,10 +1,14 @@
 'use strict';
 
 angular.module('bothSidesFrontEndApp')
-  .controller('DisputeCtrl', function ($scope) {
-    $scope.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
-  });
+    .controller('DisputeCtrl', ['$scope', function ($scope) {
+        $scope.tabs = [
+            { title:"Dynamic Title 1",
+              content:"Dynamic content 1" },
+            { title:"Dynamic Title 2",
+              content:"Dynamic content 2",
+              disabled: true }
+        ];
+
+        $scope.navType = 'pills';
+    }]);
